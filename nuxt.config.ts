f98a46d8592c9f8534993f18775643a8e62ad826
reliableweed <120849700+reliableweed@users.noreply.github.com>
2022-12-17 18:16:03 -0500

    add netlify identity to all pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   ssr: false,
+  nitro: {
+    preset: 'netlify'
+  },
   target: 'static',
   modules: ['@nuxt/content'],
   css: ["tachyons/css/tachyons.min.css"],
+  // add the netlify identity widget to the head
+  head: {
+    script: [
+      {
+        src: "https://identity.netlify.com/v1/netlify-identity-widget.js",
+        defer: true,
+      },
+    ],
+  },
 })
